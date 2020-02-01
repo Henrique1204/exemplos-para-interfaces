cd1928c43a7528d9fd0f04007151a7679ec03022
Paulo <pauloh16.pdr@gmail.com>
2020-02-01 00:56:43 -0300

    Criar menu desktop

---
+++ menu/script.js
@@ -0,0 +1,10 @@
+let logoMarca = document.querySelector('.logo_marca');
+
+function mudar_imagem(ocasiao){
+    
+    if(ocasiao == 'em cima'){
+        logoMarca.src= 'logo.svg';
+    }else{
+        logoMarca.src= 'logo_claro.svg';
+    }
+}
